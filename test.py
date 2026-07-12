73053a9474e1370aaca53a5601aff2330e42af93
myfryll <venom41k2910@gmail.com>
2026-07-12 16:25:43 +0300

    Deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from pyfiglet import Figlet
-from rich.console import Console
-
-console = Console()
-
-fig = Figlet(font="slant")
-console.print(f"[cyan]{fig.renderText('Hydra Helper')}[/cyan]")
-console.print("[bold white]by myfryll[/bold white]")
